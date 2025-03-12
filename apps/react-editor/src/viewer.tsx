29a034372735687a0b1d4f50c8c4f375dc155d63
Christo Goosen <christogoosen@gmail.com>
2025-03-12 15:59:50 +0200

    Replace library call

--- apps/react-editor/src/viewer.tsx
+++ apps/react-editor/src/viewer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Viewer, { EventMap } from '@toast-ui/editor/dist/toastui-editor-viewer';
+import Viewer, { EventMap } from '@utilitywarehouse/editor/dist/toastui-editor-viewer';
 import { ViewerProps, EventNames } from '../index';
 
 export default class ViewerComponent extends React.Component<ViewerProps> {
